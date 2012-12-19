b0bd0e376a9f0b2fbf0b8039a9145242958d7b20
kanishka mishra <kanishka.mishra@tcs.com>
2012-12-19 15:07:06 +0530

    base app and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceMatch::Application.config.secret_token = '8e7ca08fd944c0e8f1730b452261bdce61d612d7070faaa780472099a801babab5e4f653bc3f0879211f1a4eb3e4ed41a9e53ef10c2dd84fb9db9dcf259f3876'
